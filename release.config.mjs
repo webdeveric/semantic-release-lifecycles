e7b4fa72024dbc778b17c55bf5c1efad2da670cf
Eric King <eric@webdeveric.com>
2024-08-12 11:21:26 -0500

    chore(release): updated config

--- release.config.mjs
+++ release.config.mjs
@@ -3,4 +3,30 @@
  */
 export default {
   branches: ['main'],
+  preset: 'conventionalcommits',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            scope: 'spelling',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
 };
